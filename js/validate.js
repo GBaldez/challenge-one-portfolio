965cef4d337fb66f7ec9b3fa3f7f59c09d053e95
GBaldez <83733139+GBaldez@users.noreply.github.com>
2022-07-14 08:36:23 -0300

    form validation

--- js/validate.js
+++ js/validate.js
@@ -1,1 +1,78 @@
 //Seu JavaScript de validação aqui
+const form = document.querySelector('.formcontato__form');
+const inputField = document.querySelectorAll('.formcontato__item');
+const nameInput = document.getElementById('nome');
+const emailInput = document.getElementById('email');
+const subjectInput = document.getElementById('assunto');
+const msgInput = document.getElementById('mensagem');
+const button = document.querySelector('.formcontato__botao');
+
+const msgError = (el, msg, i) => {
+    el.classList.add('error');
+    const errorMessage = document.createElement('span');
+    errorMessage.classList.add('error-message');
+    errorMessage.innerHTML = msg;
+    inputField[i].appendChild(errorMessage);
+    inputField[i].children[0].style.color = 'red';
+}
+const removeErrStyle = (el, i) => {
+        el.classList.remove('error');
+        inputField[i].children[2].remove();
+        inputField[i].children[0].style.color = 'var(--second-color)';
+        button.disabled = true;
+    }
+
+const validateNameAndSubject = (input,i) => {
+     if(input.value.length > 49){
+        msgError(input, 'Máximo de 50 caracteres para este campo', i);
+     } else if(input.value.length === 0) {
+        msgError(input, 'Esse campo não pode ficar em branco', i)
+     } else if(!input.value.match(/^[a-zA-Z\s]+$/)){
+        msgError(input, 'Somente letras neste campo', i);
+     }  else {
+            removeErrStyle(input, i);
+        }
+    }
+
+const validateEmail = () => {
+    const emailValid = new RegExp( /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+    if (!emailValid.test(emailInput.value)) {
+        msgError(emailInput, 'E-mail inválido', 1); 
+    } else {
+        removeErrStyle(emailInput, 1);
+    }
+}
+const validateMsg = () => {
+    if(msgInput.value.length > 499){
+        msgError(msgInput, 'Máximo de 500 caracteres para este campo', 3);
+    } else if(msgInput.value.length === 0) {
+        msgError(msgInput, 'Esse campo não pode ficar em branco', 3);
+    } else {
+        removeErrStyle(msgInput, 3);
+    }
+}
+
+form.addEventListener('focusout', (e) => {
+    console.log(e.target.id);
+    if (e.target.id === 'nome') {
+        validateNameAndSubject(nameInput, 0);
+    }
+    if (e.target.id === 'email') {
+        validateEmail();
+    }
+    if (e.target.id === 'assunto') {
+        validateNameAndSubject(subjectInput, 2);
+    }
+    if (e.target.id === 'mensagem') {
+        validateMsg();
+    }
+}
+);
+
+button.addEventListener('click', () => {
+        validateNameAndSubject(nameInput, 0);
+        validateEmail();
+        validateNameAndSubject(subjectInput, 2);
+        validateMsg();
+});
+    
